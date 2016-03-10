2199a62b493524ddef108b46c6e3a3d53ed06697
katarina <golbang.k@gmail.com>
2016-03-10 21:24:11 +0100

    enhance tests and add some docs

--- app/music/album.component.spec.ts
+++ app/music/album.component.spec.ts
@@ -1,3 +1,4 @@
+/* stuff needed for testing, e.g. angular's wrapper functions for jasmine */
 import {
     beforeEach,
     beforeEachProviders,
@@ -8,23 +9,27 @@ import {
     injectAsync,
     TestComponentBuilder,
     ComponentFixture,
-    setBaseTestProviders
+    setBaseTestProviders,
+    FunctionWithParamTokens
 } from 'angular2/testing';
-import {provide} from 'angular2/core';
-import {AlbumComponent} from './album.component'
-import {MusicService} from "./services/music.srv";
-import {Observable} from 'rxjs/Observable';
-import {Album} from "./models/album";
 import {
     TEST_BROWSER_PLATFORM_PROVIDERS,
     TEST_BROWSER_APPLICATION_PROVIDERS
 } from 'angular2/platform/testing/browser';
+import {provide} from 'angular2/core';
 
+/**
+ * stuff needed to work with router params... (=> means "depends on")
+ */
 import { RootRouter } from 'angular2/src/router/router';
 import { Location, RouteParams, Router, RouteRegistry, ROUTER_PRIMARY_COMPONENT } from 'angular2/router';
 import { SpyLocation } from 'angular2/src/mock/location_mock';
 import { AppComponent } from '../app.component';
-import {ConnectionBackend,Http,RequestOptions} from 'angular2/http';
+
+import {AlbumComponent} from './album.component'
+import {MusicService} from "./services/music.srv";
+import {Observable} from 'rxjs/Observable';
+import {Album} from "./models/album";
 
 class MockMusicService {
     albumInfo(id: string) {
@@ -35,11 +40,41 @@ class MockMusicService {
     }
 }
 
+/**
+ * use this in the future to make test cases DRY
+ * @param testingFn
+ * @returns {FunctionWithParamTokens}
+ */
+function buildTestingFn(testingFn: Function): FunctionWithParamTokens {
+    return injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+        return tcb.createAsync(AlbumComponent).then((fixture:ComponentFixture) => {
+            testingFn.apply(this, [fixture]);
+        });
+    })
+}
+
 describe("AlbumComponent", () => {
+
+    /**
+     * Fix for exception:
+     *
+     * Failed: No provider for TestComponentBuilder!
+     * Error: DI Exception
+     * at NoProviderError.BaseException [as constructor] (/home/katarina/PhpstormProjects/mytunes/node_modules/angular2/bundles/angular2.dev.js:7351:21)
+     */
     beforeAll(() => {
         setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
     });
 
+    /**
+     * stuff needed to work with router params and MusicService as well... (=> means "depends on"):
+     *
+     * * RootRouter => registry: RouteRegistry, location: Location, primaryComponent: Type
+     * * Location can be mocked by SpyLocation
+     * * ROUTER_PRIMARY_COMPONENT is AppComponent instance
+     * * RouteParams needs to be "show" route for the album, that means id "1" is defined
+     * * MusicService is mocked by MockUserService
+     */
     beforeEachProviders(() => [
         RouteRegistry,
         provide(Location, {useClass: SpyLocation}),
@@ -75,11 +110,9 @@ describe("AlbumComponent", () => {
         });
     }));
 
-    it("should show the large cover", injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-        return tcb.createAsync(AlbumComponent).then((fixture:ComponentFixture) => {
-            fixture.detectChanges();
-            let el = fixture.nativeElement;
-            expect(el.querySelector('img').src).toMatch(/cover\.png$/);
-        });
+    it("should show the large cover", buildTestingFn((fixture: ComponentFixture) => {
+        fixture.detectChanges();
+        let el = fixture.nativeElement;
+        expect(el.querySelector('img').src).toMatch(/cover\.png$/);
     }));
 });
--- js/music/album.component.spec.js
+++ js/music/album.component.spec.js
@@ -1,30 +1,31 @@
-System.register(['angular2/testing', 'angular2/core', './album.component', "./services/music.srv", 'rxjs/Observable', "./models/album", 'angular2/platform/testing/browser', 'angular2/src/router/router', 'angular2/router', 'angular2/src/mock/location_mock', '../app.component'], function(exports_1, context_1) {
+System.register(['angular2/testing', 'angular2/platform/testing/browser', 'angular2/core', 'angular2/src/router/router', 'angular2/router', 'angular2/src/mock/location_mock', '../app.component', './album.component', "./services/music.srv", 'rxjs/Observable', "./models/album"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var testing_1, core_1, album_component_1, music_srv_1, Observable_1, album_1, browser_1, router_1, router_2, location_mock_1, app_component_1;
+    var testing_1, browser_1, core_1, router_1, router_2, location_mock_1, app_component_1, album_component_1, music_srv_1, Observable_1, album_1;
     var MockMusicService;
+    /**
+     * use this in the future to make test cases DRY
+     * @param testingFn
+     * @returns {FunctionWithParamTokens}
+     */
+    function buildTestingFn(testingFn) {
+        var _this = this;
+        return testing_1.injectAsync([testing_1.TestComponentBuilder], function (tcb) {
+            return tcb.createAsync(album_component_1.AlbumComponent).then(function (fixture) {
+                testingFn.apply(_this, [fixture]);
+            });
+        });
+    }
     return {
         setters:[
             function (testing_1_1) {
                 testing_1 = testing_1_1;
             },
-            function (core_1_1) {
-                core_1 = core_1_1;
-            },
-            function (album_component_1_1) {
-                album_component_1 = album_component_1_1;
-            },
-            function (music_srv_1_1) {
-                music_srv_1 = music_srv_1_1;
-            },
-            function (Observable_1_1) {
-                Observable_1 = Observable_1_1;
-            },
-            function (album_1_1) {
-                album_1 = album_1_1;
-            },
             function (browser_1_1) {
                 browser_1 = browser_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             },
             function (router_1_1) {
                 router_1 = router_1_1;
@@ -37,6 +38,18 @@ System.register(['angular2/testing', 'angular2/core', './album.component', "./se
             },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
+            },
+            function (album_component_1_1) {
+                album_component_1 = album_component_1_1;
+            },
+            function (music_srv_1_1) {
+                music_srv_1 = music_srv_1_1;
+            },
+            function (Observable_1_1) {
+                Observable_1 = Observable_1_1;
+            },
+            function (album_1_1) {
+                album_1 = album_1_1;
             }],
         execute: function() {
             MockMusicService = (function () {
@@ -50,9 +63,25 @@ System.register(['angular2/testing', 'angular2/core', './album.component', "./se
                 return MockMusicService;
             }());
             testing_1.describe("AlbumComponent", function () {
+                /**
+                 * Fix for exception:
+                 *
+                 * Failed: No provider for TestComponentBuilder!
+                 * Error: DI Exception
+                 * at NoProviderError.BaseException [as constructor] (/home/katarina/PhpstormProjects/mytunes/node_modules/angular2/bundles/angular2.dev.js:7351:21)
+                 */
                 beforeAll(function () {
                     testing_1.setBaseTestProviders(browser_1.TEST_BROWSER_PLATFORM_PROVIDERS, browser_1.TEST_BROWSER_APPLICATION_PROVIDERS);
                 });
+                /**
+                 * stuff needed to work with router params and MusicService as well... (=> means "depends on"):
+                 *
+                 * * RootRouter => registry: RouteRegistry, location: Location, primaryComponent: Type
+                 * * Location can be mocked by SpyLocation
+                 * * ROUTER_PRIMARY_COMPONENT is AppComponent instance
+                 * * RouteParams needs to be "show" route for the album, that means id "1" is defined
+                 * * MusicService is mocked by MockUserService
+                 */
                 testing_1.beforeEachProviders(function () { return [
                     router_2.RouteRegistry,
                     core_1.provide(router_2.Location, { useClass: location_mock_1.SpyLocation }),
@@ -82,12 +111,10 @@ System.register(['angular2/testing', 'angular2/core', './album.component', "./se
                         testing_1.expect(el.querySelector('h1>small')).toHaveText("Linkin Park");
                     });
                 }));
-                testing_1.it("should show the large cover", testing_1.injectAsync([testing_1.TestComponentBuilder], function (tcb) {
-                    return tcb.createAsync(album_component_1.AlbumComponent).then(function (fixture) {
-                        fixture.detectChanges();
-                        var el = fixture.nativeElement;
-                        testing_1.expect(el.querySelector('img').src).toMatch(/cover\.png$/);
-                    });
+                testing_1.it("should show the large cover", buildTestingFn(function (fixture) {
+                    fixture.detectChanges();
+                    var el = fixture.nativeElement;
+                    testing_1.expect(el.querySelector('img').src).toMatch(/cover\.png$/);
                 }));
             });
         }
